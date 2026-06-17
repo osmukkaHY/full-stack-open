c64318bd5d55e093ced7babd2917204ebb220bad
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-06-17 18:40:24 +0300

    1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
+const Button = ({text, callback}) => <button onClick={callback}>{text}</button>
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <>
+      {text}: {value} <br/> 
+    </>
+  )
+}
+
 const Statistics = ({good, neutral, bad}) => {
   const getAll = () => good + neutral + bad
 
   const getAverage = () => (good - bad) / getAll()
 
-  const getPositive = () => good / (getAll())
+  const getPositive = () => good / getAll()
 
   if(getAll() === 0)
     return (
@@ -18,16 +28,12 @@ const Statistics = ({good, neutral, bad}) => {
   return (
   <>
     <h1>Statistics</h1>
-    <p>
-      Good: {good} <br/>
-      Neutral: {neutral} <br/>
-      Bad: {bad}
-    </p>
-    <p>
-      All: {getAll()} <br/>
-      Average: {getAverage()} <br/>
-      Positive: {getPositive()}%
-    </p>
+      <StatisticLine text={'Good'} value={good} />
+      <StatisticLine text={'Neutral'} value={neutral} />
+      <StatisticLine text={'Bad'} value={bad} />
+      <StatisticLine text={'All'} value={getAll()} />
+      <StatisticLine text={'Average'} value={getAverage()} />
+      <StatisticLine text={'Positive'} value={getPositive()} />
   </>
   )
 }
@@ -37,23 +43,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodPress = () => {
-    setGood(good + 1)
-  }
+  const handleGoodPress = () => setGood(good + 1)
+  const handleNeutralPress = () => setNeutral(neutral + 1)
+  const handleBadPress = () => setBad(bad + 1)
 
-  const handleNeutralPress = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handleBadPress = () => {
-    setBad(bad + 1)
-  }
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGoodPress}>good</button>
-      <button onClick={handleNeutralPress}>neutral</button>
-      <button onClick={handleBadPress}>bad</button>
+      <Button text='good' callback={handleGoodPress} />
+      <Button text='neutral' callback={handleNeutralPress} />
+      <Button text='bad' callback={handleBadPress} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
