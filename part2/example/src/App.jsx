ad376903ba652cdbe77083d96b488382e9e2126c
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-06-26 17:15:51 +0300

    Added useeffect

--- part2/example/src/App.jsx
+++ part2/example/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Note from './components/Note.jsx'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
