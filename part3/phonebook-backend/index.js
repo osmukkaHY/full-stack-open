b1b2d9659c673de5fa9aaf9699a6a5b69e7909be
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-06-30 16:06:17 +0300

    3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -36,7 +36,14 @@ app.get("/api/persons/:id", (req, res) => {
     else {
         res.status(404).end();
     }
-})
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(3001);
