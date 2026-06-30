195d9a080f550bb10a34ebed9480babdf405d717
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-06-30 15:36:46 +0300

    Added saving new notes

--- part3/example/index.js
+++ part3/example/index.js
@@ -21,6 +21,13 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -40,8 +47,19 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    body = request.body
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+    notes = notes.concat(note)
     response.json(note)
 })
 
