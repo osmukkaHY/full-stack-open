4fcd1ce62e0dd4f7c32fc4103e19b004707f11c6
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-01 14:53:51 +0300

    3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const app = express();
 
 morgan.token("postcontent", function(req, res) {return req.method === "POST" ? JSON.stringify(req.body) : "-"});
-app.use(cors())
+app.use(express.static("dist"));
+app.use(cors());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :postcontent"));
 app.use(express.json());
 
