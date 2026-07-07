9a9a4d4fd5a6650f688693501ea6766a6bea4e7d
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-07 11:52:42 +0300

    4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,15 +1,32 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.js");
+const User = require("../models/user.js");
 
 blogsRouter.get("/", async (req, res) => {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user");
     res.json(blogs);
 });
 
 blogsRouter.post("/", async (req, res) => {
-    const blog = new Blog(req.body);
+    console.log(await User.find({})[0]);
+    const user = (await User.find({}))[0];
+    console.log(user)
+    if(!user) {
+        return res.status(404).json({error: "No users found."});
+    }
 
-    await blog.save();
+    const blog = new Blog({
+        title: req.body.title,
+        author: req.body.author,
+        url: req.body.url,
+        likes: req.body.likes,
+        user: user._id
+    });
+
+    const savedBlog = await blog.save();
+    console.log(savedBlog);
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     res.status(201).json(blog);
 });
 
