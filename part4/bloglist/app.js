43376f99a099034b3526d2c7f6d299bd4cf63b53
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-07 10:06:47 +0300

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const config = require("./utils/config.js");
 const logger = require("./utils/logger.js");
 const middleWare = require("./utils/middleware.js");
-const blogsRouter = require("./controllers/blogs");
+const blogsRouter = require("./controllers/blogs.js");
+const usersRouter = require("./controllers/users.js");
 
 const app = express();
 
@@ -15,6 +16,7 @@ mongoose
 app.use(express.json());
 app.use(middleWare.requestLogger);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleWare.unknownEndpoint);
 app.use(middleWare.errorHandler);
 
