f4f5aa6dda5beb39ac06b4cc1eef7ba0c1e2ad42
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-08 09:58:45 +0300

    4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const logger = require("./utils/logger.js");
 const middleWare = require("./utils/middleware.js");
 const blogsRouter = require("./controllers/blogs.js");
 const usersRouter = require("./controllers/users.js");
+const loginRouter = require("./controllers/login.js");
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(middleWare.requestLogger);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleWare.unknownEndpoint);
 app.use(middleWare.errorHandler);
 
