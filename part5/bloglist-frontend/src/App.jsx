95323fef7b1de31d352b71a92cf8218de127655d
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-07-09 13:22:45 +0300

    5.1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from "./services/login.js"
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService
+      .getAll().
+      then(blogs => setBlogs(blogs))  
+  }, []);
+
+  const handleLogin = async e => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({username, password});
+      setUser(user);
+      setUsername("");
+      setPassword("");
+      console.log(blogs);
+    }
+    catch {
+      console.log("incorrect credentials");
+      //setErrorMessage("wrong credentials");
+      //setTimeout(() => {
+      //  setErrorMessage(null);
+      //}, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <>
+    <h2>Login</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({target}) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+    </>
+  );
+
+  const greeting = () => (
+    <p>Hello, {user.name}</p>
+  )
+
+  const blogsList = () => (
+    <>
+    {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )}
+    </>
+  )
+
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {!user && loginForm()}
+      {user && greeting()}
+      {user && blogsList()}
+    </div>
+  )
+}
+
+export default App
